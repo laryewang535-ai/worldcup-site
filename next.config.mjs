3bad8d573be5f2cf9b6513342cc7016849aba70b
ym <ym@ymdeMac-mini.local>
2026-06-06 17:37:00 +0800

    Add SEO URL redirects

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,30 @@
 const nextConfig = {
   reactStrictMode: true,
   compress: true,
+  async redirects() {
+    return [
+      {
+        source: "/teams/usa",
+        destination: "/teams/united-states",
+        permanent: true,
+      },
+      {
+        source: "/cities/new-york-new-jersey-world-cup-2026-guide",
+        destination: "/cities/new-york-new-jersey",
+        permanent: true,
+      },
+      {
+        source: "/cities/los-angeles-world-cup-2026-guide",
+        destination: "/cities/los-angeles",
+        permanent: true,
+      },
+      {
+        source: "/cities/miami-world-cup-2026-guide",
+        destination: "/cities/miami",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
